604d46386ad1680a4b6207fd9816d9b51b167425
Serge Villasica <villasica.serge@gmail.com>
2019-04-10 11:24:26 +0800

    Add progress bar template

---
+++ app/src/main/java/com/serge/basicweatherapp/ui/ProgressBarDialog.kt
@@ -0,0 +1,34 @@
+package com.serge.basicweatherapp.ui
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.os.Bundle
+import com.serge.basicweatherapp.R
+
+const val PROGRESS_BAR_TEXT = "PROGRESS_BAR_DIALOG"
+
+class ProgressBarDialog : androidx.fragment.app.DialogFragment() {
+
+    companion object {
+        const val PROGRESS_BAR_TAG = "PROGRESS_BAR_TAG"
+
+        fun newInstance(text: String?): ProgressBarDialog {
+            val progressBarDialog = ProgressBarDialog()
+            val bundle = Bundle()
+            bundle.putString(PROGRESS_BAR_TEXT, text)
+            progressBarDialog.arguments = bundle
+            return progressBarDialog
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        isCancelable = false
+        val dialog = Dialog(activity!!, R.style.ProgressDialogTheme)
+        @SuppressLint("InflateParams") val view =
+            activity!!.layoutInflater.inflate(R.layout.dialog_progress_circular, null, false)
+        dialog.setContentView(view)
+        dialog.setCancelable(false)
+        dialog.setCanceledOnTouchOutside(false)
+        return dialog
+    }
+}
